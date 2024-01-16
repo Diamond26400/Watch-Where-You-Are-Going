5ef4c7cb076ce76af2c1ad0fe842f182f867f8ed
Diamond26400 <obaloluwajames5@gmail.com>
2024-01-16 19:29:33 +0100

    Updare

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
